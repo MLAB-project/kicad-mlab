87294e613ca75f94db655b65c43a319a1f517d23
Ondřej Šesták <ondra6ak@gmail.com>
2017-11-27 10:58:41 +0100

    Install script fixed.

--- install-all.sh
+++ install-all.sh
@@ -4,7 +4,8 @@ sudo cp src/repo.py /usr/lib/python3/dist-packages/repo.py
 sudo chmod +x src/kicad-footprint-manager
 sudo cp src/kicad-footprint-manager /usr/bin/kicad-footprint-manager
 
-cp -r footprints ~/kicad_sources
+mkdir -p ~/kicad_sources
+cp -r footprints ~/kicad_sources/
 sudo cp -r symbols /usr/share/kicad/library
 sudo cp -r template /usr/local/share/kicad
 sudo cp -r template /usr/share/kicad
