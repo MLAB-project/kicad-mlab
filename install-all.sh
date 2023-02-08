3bca6dbfb69d779b00052b9f998b65ad70f63f6a
Jakub Kákona <kaklik@mlab.cz>
2023-02-08 18:49:45 +0000

    Update script to install Symbols, Footprints, Scripts.
    Change footprint type to SMD.

--- install-all.sh
+++ install-all.sh
@@ -8,6 +8,9 @@
 #cp -r footprints ~/kicad_sources/
 #sudo cp -r symbols /usr/share/kicad/library/
 ln -s "$(realpath template/mlab-default/)" ~/.local/share/kicad/6.0/template/
+echo $(realpath plugins/*.py) | tr " " "\n" | xargs -I % cp -s % ~/.local/share/kicad/6.0/plugins/
+echo $(realpath footprints/*.pretty) | tr " " "\n" | xargs -I % ln -s % ~/.local/share/kicad/6.0/footprints/
+echo $(realpath symbols/*.kicad_sym) | tr " " "\n" | xargs -I % cp -s % ~/.local/share/kicad/6.0/symbols/
 #sudo cp -r modules /usr/share/kicad/
 
 #kicad-footprint-manager  -f ~/kicad_sources/footprints
